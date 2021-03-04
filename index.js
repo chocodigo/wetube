3f035d8299c8799c291bd724b9ff3a316a2419bd
chocodigo <cocoa1149@gmail.com>
2021-03-04 22:41:21 +0900

    + 20210304

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express"); // express 모듈을 불러옴(import)
+import express from "express"; // express 모듈을 불러옴(import)
 const app = express(); // express 어플리케이션을 실행함
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+};
 
-function handleHome(req, res) {
+const handleHome = (req, res) => {
   res.send("Hello from home");
-}
+};
 
-function handleProfile(req, res) {
+const handleProfile = (req, res) => {
   res.send("You are on my profile");
-}
+};
 
 app.get("/", handleHome);
 
