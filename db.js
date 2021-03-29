1e26e7416d2a16f501ec4d919f8113f7225f00e5
chocodigo <cocoa1149@gmail.com>
2021-03-29 23:30:10 +0900

    + 20210329 (집)

--- db.js
+++ db.js
@@ -1,62 +1,15 @@
-export const videoList = [
-  {
-    id: 123132,
-    title: "DNA",
-    description: "this is something I love",
-    views: 24,
-    videoFile: "https://www.youtube.com/embed/MBdVXkSdhwU",
-    creator: {
-      name: "BTS",
-      email: "bts@las.com",
-      id: 121212,
-    },
-  },
-  {
-    id: 43535,
-    title: "Boy With Luv",
-    description: "this is something I love",
-    views: 24,
-    videoFile: "https://youtu.be/XsX3ATc3FbA",
-    creator: {
-      name: "BTS",
-      email: "bts@las.com",
-      id: 121212,
-    },
-  },
-  {
-    id: 435123,
-    title: "Dynamite",
-    description: "this is something I love",
-    views: 24,
-    videoFile: "https://youtu.be/gdZLi9oWNZg",
-    creator: {
-      name: "BTS",
-      email: "bts@las.com",
-      id: 121212,
-    },
-  },
-  {
-    id: 789766,
-    title: "Life Goes On",
-    description: "this is something I love",
-    views: 24,
-    videoFile: "https://youtu.be/-5q5mZbe3V8",
-    creator: {
-      name: "BTS",
-      email: "bts@las.com",
-      id: 121212,
-    },
-  },
-  {
-    id: 55555,
-    title: "ON",
-    description: "this is something I love",
-    views: 24,
-    videoFile: "https://youtu.be/mPVDGOVjRQ0",
-    creator: {
-      name: "BTS",
-      email: "bts@las.com",
-      id: 121212,
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = () => console.log(`❌ Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleOpen);
