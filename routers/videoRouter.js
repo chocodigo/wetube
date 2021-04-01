c5cd746fdc8cabe054237cc754ec83a8ae1def1d
최해림 <chocodigo1149@gmail.com>
2021-04-01 17:05:54 +0900

    + 20210401 회사

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,7 +6,8 @@ import {
   getUpload,
   postUpload,
   videoDetail,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
   deleteVideo,
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
@@ -15,10 +16,19 @@ const videoRouter = express.Router();
 
 videoRouter.get(routes.home, home);
 videoRouter.get(routes.videos, videos);
+
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo(), editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
